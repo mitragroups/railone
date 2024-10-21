71b7d70e4d3fea1c9ed4a3f93746a462c94e5d5a
mitragroups <miftahmuhajir@gmail.com>
2024-10-21 21:40:31 +0700

    Add [demo#iseng, demo#cak, demo#coba]

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -1,4 +1,17 @@
 class DemoController < ApplicationController
   def index
   end
+
+  def iseng
+    render plain: 'Hello World!'
+  end
+
+  def cak
+    render 'demo/coba'
+  end
+
+  def coba
+    render plain: 'JSON Coba'
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   get 'demo/index'
+  get 'demo/iseng'
+  get 'demo/cak'
+  get 'demo/coba'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
