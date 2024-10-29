f8b84a909e1557f49a80624a22481d5b2b64546b
mitragroups <miftahmuhajir@gmail.com>
2024-10-29 08:05:24 +0700

    Add method cheaper & equal_more in book#model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,9 @@
 class Book < ApplicationRecord
+  def self.cheaper
+    order(price: :asc).pluck(:title, :price)
+  end
+
+  def self.equal_more(price)
+    where('price >= ?', price).pluck(:title, :price)
+  end
 end
