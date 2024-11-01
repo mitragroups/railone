839fdd21ab0dee8eb21f5cebc8d77046b71bbdb7
mitragroups <miftahmuhajir@gmail.com>
2024-11-01 13:50:43 +0700

    Add data authors

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,3 +12,7 @@ Book.create(title: 'Flutter', price: 150_000, page: 150, description: 'Bagus unt
 Book.create(title: 'Go Lang', price: 75_000, page: 75, description: 'Agak sulit karena untuk backend dan functional programming')
 Book.create(title: 'Springboot', price: 800_000, page: 800, description: 'Super Lengkap untuk Framework Springboot Java')
 =end
+
+Author.create(name: 'Mahreen', age: 4, gender: 'female', address: 'Lahir di Bogor')
+Author.create(name: "Mu'ammar", age: 9, gender: 'male', address: 'Anak laki - laki yang hebat dan baik budi')
+Author.create(name: 'Mafaaza', age: 6, gender: 'female', address: 'Lahir di Tinggi')
