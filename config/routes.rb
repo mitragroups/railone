984ebb4914a583e76d6d8f3bd11f07836e2ec057
mitragroups <miftahmuhajir@gmail.com>
2024-11-02 10:17:20 +0700

    Add oldest in view and controller

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
   get 'home/index'
   get 'home/rumah'
+  get 'home/tua'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
